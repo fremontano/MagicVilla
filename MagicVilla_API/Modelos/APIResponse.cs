4519a63b44468117b75def8f1c2c7115a215da84
freyder_montano <fremontano24@gmail.com>
2024-02-16 20:21:11 -0300

    Creando APIRespone para estandarizar las respuestas devueltas por los endpoints de la API

---
+++ MagicVilla_API/Modelos/APIResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace MagicVilla_API.Modelos
+
+
+    //para qeu todos nuestros enpoint retornen una respuesta estandar, en representacion a lo que queremos retornar
+{
+    public class APIResponse
+    {
+
+        //propiedades
+        public HttpStatusCode statusCode { get; set; } //codigo de estado que retorne mi enpoint
+        public bool IsExistoso { get; set; } = true;
+
+
+        //una lista string donde vamos a almacenar todos los errores que se nos presentan
+        public List<string> ErrorMessages { get; set; }
+
+
+        //Propieda de tipo objeto, para almacenar cualquier tipo de lista,objeto cualquier tipo 
+        public object Resultado { get; set; }
+
+
+    }
+}
